0b7b656f2d3e1b97c1d3de4b80e51baef86fbca7
basilgood <elsile69@yahoo.com>
2018-08-06 17:28:00 +0300

    add path-from-shell

--- configs/repositories.el
+++ configs/repositories.el
@@ -1,6 +1,10 @@
+;;; repositories.el --- summary
+;;; commentary:
+;;; code:
+
 ;;; This fixed garbage collect, makes emacs start up faster
 (setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
+  gc-cons-percentage 0.6)
 
 (defvar startup/file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
@@ -10,11 +14,10 @@
 
 (defun startup/reset-gc ()
   (setq gc-cons-threshold 16777216
-	gc-cons-percentage 0.1))
+	  gc-cons-percentage 0.1))
 
 (add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 (add-hook 'emacs-startup-hook 'startup/reset-gc)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; This is all kinds of necessary
 (require 'package)
@@ -25,9 +28,17 @@
                           ("melpa" . "https://melpa.org/packages/")
                           ("org"   . "https://orgmode.org/elpa/")))
 (package-initialize)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; Bootstrapping use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+;;; path-from-shell
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+;;; repositories.el ends here
