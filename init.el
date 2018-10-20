5c70729f949e580165d5cccf0cb6eb8c1b5e94c0
vasile <elsile69@yahoo.com>
2018-10-20 21:00:34 +0300

    Org mode configuration

--- init.el
+++ init.el
@@ -1,20 +1,40 @@
-;; package --- summary
+;;; repositories.el --- summary
 ;;; commentary:
 ;;; code:
 
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file 'noerror)
+;;; This fixed garbage collect, makes emacs start up faster
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
 
-(load-file "~/.emacs.d/configs/repositories.el")
-(load-file "~/.emacs.d/configs/options.el")
-(load-file "~/.emacs.d/configs/theme.el")
-(load-file "~/.emacs.d/configs/global.el")
-(load-file "~/.emacs.d/configs/editorconfig.el")
-(load-file "~/.emacs.d/configs/packages.el")
-(load-file "~/.emacs.d/configs/navigation.el")
-(load-file "~/.emacs.d/configs/treemacs.el")
-(load-file "~/.emacs.d/configs/speedbar-extension.el")
-(load-file "~/.emacs.d/configs/lang.el")
-(load-file "~/.emacs.d/configs/lint.el")
-(load-file "~/.emacs.d/configs/eshell.el")
-;;; init.el ends here
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+        gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+;;; This is all kinds of necessary
+(require 'package)
+(setq package-enable-at-startup nil)
+
+;;; remove SC if you are not using sunrise commander and org if you like outdated packages
+(setq package-archives '(("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+
+(add-to-list 'exec-path "~/.nix-profile/bin")
+
+;;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
