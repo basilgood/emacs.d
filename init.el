b815c63e821d8ed2b9149298ca1abce6921699ee
vasile <elsile69@yahoo.com>
2018-10-22 05:51:35 +0000

    Better configuration org mode

--- init.el
+++ init.el
@@ -1,40 +1,15 @@
-;;; repositories.el --- summary
-;;; commentary:
-;;; code:
-
-;;; This fixed garbage collect, makes emacs start up faster
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
-
-(defvar startup/file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
-
-(defun startup/revert-file-name-handler-alist ()
-  (setq file-name-handler-alist startup/file-name-handler-alist))
-
-(defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-        gc-cons-percentage 0.1))
-
-(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
-
-;;; This is all kinds of necessary
-(require 'package)
-(setq package-enable-at-startup nil)
-
-;;; remove SC if you are not using sunrise commander and org if you like outdated packages
-(setq package-archives '(("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa/")))
-(package-initialize)
-
-(add-to-list 'exec-path "~/.nix-profile/bin")
-
-;;; Bootstrapping use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;;; Load the config
-(org-babel-load-file (concat user-emacs-directory "config.org"))
+(require 'org-install)
+(require 'ob-tangle)
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
