af9254a61d182e4251b31d428d5ec3887a9f56c0
vasile <elsile69@yahoo.com>
2018-10-31 05:28:30 +0000

    Refactor init.el

--- init.el
+++ init.el
@@ -1,4 +1,50 @@
-;; (package-initialize)
-(require 'org-install)
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+;;; = init.el =
+;;; Commentary
+;;; Code
+
+(eval-when-compile (require 'cl))
+(lexical-let ((emacs-start-time (current-time)))
+  (add-hook 'emacs-startup-hook
+            (lambda ()
+              (let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
+                (message "[Emacs initialized in %.3fs]" elapsed)))))
+
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
+
+(defun startup/reset-gc ()
+  (setq gc-cons-threshold 16777216
+	      gc-cons-percentage 0.1))
+
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
+(add-hook 'emacs-startup-hook 'startup/reset-gc)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+			                   ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+
+(setq tls-checktrust t)
+(setq gnutls-verify-error t)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;;; --init.el ends here
