840bcfc1246410280d5bca3da386c91dda321734
vasile luta <elsile69@yahoo.com>
2019-07-29 06:52:04 +0300

    theme:::diminish whitespace

--- elisp/theme.el
+++ elisp/theme.el
@@ -1,7 +1,5 @@
-;;; base-theme.el --- Themes for graphical and terminal sessions
-;;
+;;; theme.el --- Themes for graphical and terminal sessions
 ;;; Commentary:
-
 ;;; Code:
 
 (use-package color-theme-sanityinc-tomorrow
@@ -39,6 +37,7 @@
   (dolist (hook '(prog-mode-hook text-mode-hook))
     (add-hook hook #'whitespace-mode))
   (add-hook 'before-save-hook #'whitespace-cleanup)
+  :diminish whitespace ""
   :config
   (setq whitespace-line-column 80) ;; limit line length
   (setq whitespace-style '('spaces tabs space-mark tab-mark)))(provide 'theme)
