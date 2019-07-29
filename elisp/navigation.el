76ff9be1dd277ed554b68d89a9d8d13a20bc08ae
vasile luta <elsile69@yahoo.com>
2019-07-29 11:05:35 +0300

    navigation:::projectile, perspective and eyebrowse

--- elisp/navigation.el
+++ elisp/navigation.el
@@ -23,11 +23,11 @@
   :hook (helm-flx-mode)
   :hook (helm-fuzzier-mode)
   :diminish helm-mode ""
-  :bind (("M-x" . helm-smex)
+  :bind ("M-x" . helm-smex)
           ("C-x C-f" . helm-find-files)
           ("C-x f" . helm-recentf)
           ("M-y" . helm-show-kill-ring)
-          ("C-x b" . helm-buffers-list)))
+          ("C-x b" . helm-buffers-list))
 
 (use-package helm-files
   :straight nil
@@ -38,7 +38,41 @@
   helm-display-buffer-default-height 20
   helm-default-display-buffer-functions '(display-buffer-in-side-window))
 
+(use-package projectile
+  :straight t
+  :init
+  (setq projectile-completion-system 'helm)
+  (projectile-global-mode)
+  :bind
+  ("s-p" . projectile-find-file)
+  ("s-g" . projectile-ag)
+  ("s-q" . projectile-replace))
+
+(use-package persp-projectile
+  :straight t
+  :init
+  (persp-mode)
+  :bind (:map projectile-mode-map
+          ("s-n" . projectile-persp-switch-project)))
+
 (use-package neotree :straight t)
+
+(use-package eyebrowse
+  :straight t
+  :init
+  (eyebrowse-mode)
+  :config
+  (progn
+    (define-key eyebrowse-mode-map (kbd "M-0") 'eyebrowse-switch-to-window-config-0)
+    (define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+    (define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+    (define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+    (define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+    (define-key eyebrowse-mode-map (kbd "M-5") 'eyebrowse-switch-to-window-config-5)
+    (define-key eyebrowse-mode-map (kbd "M-6") 'eyebrowse-switch-to-window-config-6)
+    (define-key eyebrowse-mode-map (kbd "M-7") 'eyebrowse-switch-to-window-config-7)))
+(setq eyebrowse-mode-line-separator " "
+  eyebrowse-new-workspace t)
 
 (provide 'navigation)
 ;; Local Variables:
