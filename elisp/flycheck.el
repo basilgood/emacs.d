ec26c4d41198bbee358258e048a47d1c56fca3cc
vasile luta <elsile69@yahoo.com>
2019-07-31 18:57:34 +0300

    flycheck:::simple

--- elisp/flycheck.el
+++ elisp/flycheck.el
@@ -5,20 +5,10 @@
 (use-package flycheck
   :straight t
   :diminish  flycheck ""
-  ;; :commands flycheck-mode
   :init (global-flycheck-mode))
-  ;;:config
-  ;; (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
-  ;; :straight flycheck-color-mode-line
-  ;; :demand
-  ;; (flycheck-color-mode-line-mode))
-
-  ;; (when (maybe-require-package 'flycheck-color-mode-line)
-  ;;   (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
-
-;; (setq-default flycheck-disabled-checkers '(javascript-jshint))
-;; (setq flycheck-eslintrc "./.eslintrc.json")
-;; (setq flycheck-checkers '(javascript-eslint))
 
 (provide 'flycheck)
+;; Local Variables:
+;; byte-compile-warnings: (not free-vars)
+;; End:
 ;;; flycheck.el ends here
