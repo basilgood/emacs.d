bbf69f537cf002e1999db4dcf1d3c615d28e046c
vasile luta <elsile69@yahoo.com>
2019-08-14 21:06:17 +0300

    Literate configuration

--- init.el
+++ init.el
@@ -1,49 +1,17 @@
-;;; init.el --- init file
+;;; Package --- Summary
+
 ;;; Commentary:
-;;; Code:
+;; Emacs init file responsible for either loading a pre-compiled configuration
+;; file or tangling and loading a literate org configuration file.
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+;; Don't attempt to find/apply special file handlers to files loaded during
+;; startup.
+(let ((file-name-handler-alist nil))
+  ;; If config is pre-compiled, then load that
+  (if (file-exists-p (expand-file-name "config.elc" user-emacs-directory))
+      (load-file (expand-file-name "config.elc" user-emacs-directory))
+    ;; Otherwise use org-babel to tangle and load the configuration
+    (require 'org)
+    (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
 
-(straight-use-package 'use-package)
-(use-package use-package-ensure-system-package
-  :straight t)
-(setq use-package-always-defer t)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-
-(setq gc-cons-threshold (* 50 1000 1000))
-
-(add-to-list 'load-path (concat user-emacs-directory "elisp"))
-
-(setq ad-redefinition-action 'accept)
-
-(require 'base)
-(require 'theme)
-(require 'utils)
-(require 'navigation)
-(require 'flycheck)
-(require 'lang)
-
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
-
-(provide 'init)
-
-;; Local Variables:
-;; byte-compile-warnings: (not free-vars)
-;; End:
 ;;; init.el ends here
