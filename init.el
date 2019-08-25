12dcc2ec5d1d895629288853dba608534b4d5e37
vasile luta <elsile69@yahoo.com>
2019-08-25 14:33:47 +0300

    undo-tree simple

--- init.el
+++ init.el
@@ -296,16 +296,7 @@
 (use-package undo-tree
   :ensure t
   :diminish (undo-tree-mode . "")
-  :bind (:map undo-tree-map ("C-x u" . hydra-undo-tree/body))
-  :init (defhydra hydra-undo-tree (:hint nil)
-          "
-  _p_: undo  _n_: redo _s_: save _l_: load   "
-          ("p"   undo-tree-undo)
-          ("n"   undo-tree-redo)
-          ("s"   undo-tree-save-history)
-          ("l"   undo-tree-load-history)
-          ("u"   undo-tree-visualize "visualize" :color blue)
-          ("q"   nil "quit" :color blue))
+  :bind (:map undo-tree-map ("C-x u" . undo-tree-visualize))
   :config
   (global-undo-tree-mode))
 
