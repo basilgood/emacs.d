6d61ac6486160ee5c93df83796a53c07d4186bd3
vasile luta <elsile69@yahoo.com>
2020-07-08 08:57:20 +0300

    defaults: init.el updates

--- init.el
+++ init.el
@@ -7,13 +7,13 @@
 ;;; https://github.com/raxod502/straight.el/blob/develop/README.md#getting-started
 (defvar bootstrap-version)
 (let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+       (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
+      (url-retrieve-synchronously
+        "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+        'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
@@ -22,54 +22,58 @@
 ;;; https://github.com/raxod502/straight.el/blob/develop/README.md#integration-with-use-package
 (straight-use-package 'use-package)
 (setq  straight-use-package-by-default t)
-(setq use-package-always-defer t)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (setq ad-redefinition-action 'accept)
 
-(use-package no-littering
-  :demand t)
+(use-package no-littering)
+
 (use-package blackout
-  :straight (:host github :repo "raxod502/blackout")
-  :demand t)
-(use-package bind-key
-  :demand t)
+  :straight (:host github :repo "raxod502/blackout"))
 
-;; Supress GUI features
-(setq inhibit-startup-message t)
+(use-package bind-key)
 
-;; No backup files
+;; backup files
 (setq make-backup-files nil
-      auto-save-default nil)
+  auto-save-default nil)
 
-;; Disable cursor blink
+;; cursor blink
 (blink-cursor-mode 0)
 
 ;; smooth scroll & friends
 (setq mouse-wheel-progressive-speed nil
-      scroll-conservatively 10000
-      scroll-preserve-screen-position 'always
-      auto-window-vscroll nil)
+  scroll-conservatively 10000
+  scroll-preserve-screen-position 'always
+  auto-window-vscroll nil)
 
 ;; DONT move points out of eyes
 (setq mouse-yank-at-point t)
 
-;; No tabs
+;; misc
 (setq-default inhibit-startup-message t
-              initial-scratch-message ";; <3 "
-              inhibit-startup-message t
-              inhibit-startup-screen t
-              backward-delete-char-untabify-method nil
-              window-combination-resize t
-              tab-width 2
-              indent-tabs-mode nil)
+  initial-scratch-message ";; <3 "
+  inhibit-startup-message t
+  inhibit-startup-screen t
+  backward-delete-char-untabify-method nil
+  window-combination-resize t
+  tab-width 2
+  indent-tabs-mode nil)
 
 (prefer-coding-system 'utf-8)
 (setq enable-recursive-minibuffers t)
 
 ;; font
-(set-face-attribute 'default nil :family "Monospace" :height 120)
+(defun fontify-frame (frame)
+ "FRAME."
+ (interactive)
+  (if window-system
+    (progn
+      (if (> (x-display-pixel-width) 2000)
+        (set-frame-parameter frame 'font "DejaVuSansMono Nerd Font 14")
+        (set-frame-parameter frame 'font "DejaVuSansMono Nerd Font 11")))))
+(fontify-frame nil)
+(push 'fontify-frame after-make-frame-functions)
 
 ;; Paragraphs
 (setq sentence-end "\\([。、！？]\\|……\\|[,.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
@@ -79,38 +83,49 @@
 ;;; Macros
 (fset 'yes-or-no-p 'y-or-n-p)
 (fset 'split-horizontal-2-3
-      [?\C-x ?3 ?\C-u ?1 ?5 ?\M-x ?e ?n ?l ?a ?r tab ?- ?h ?o ?r tab return])
+  [?\C-x ?3 ?\C-u ?1 ?5 ?\M-x ?e ?n ?l ?a ?r tab ?- ?h ?o ?r tab return])
 
-;; Highlight parenthesises
+(use-package subword
+  :ensure nil
+  :config (global-subword-mode))
+
 (use-package paren
-  :demand t
-  :config (show-paren-mode)
-  :init
-  (setq show-paren-when-point-inside-paren t
-        show-paren-when-point-in-periphery t))
+  :ensure nil
+  :custom
+  (show-paren-delay 0)
+  (show-paren-when-point-inside-paren t)
+  (show-paren-when-point-in-periphery t)
+  :config (show-paren-mode))
 
-;; The selected region of text can be deleted
 (use-package delsel
-  :hook ((prog-mode text-mode) . delete-selection-mode))
+  :ensure nil
+  :config (delete-selection-mode))
 
 (use-package windmove
-  :config
-  (windmove-default-keybindings)
-  (when (fboundp 'windmove-display-default-keybindings)
-    (windmove-display-default-keybindings))
-  (when (fboundp 'windmove-delete-default-keybindings)
-    (windmove-delete-default-keybindings)))
+  :ensure nil
+  :config (windmove-default-keybindings))
 
 (use-package winner
-  :demand t
+  :ensure nil
   :config
   (winner-mode +1))
 
 (use-package transpose-frame)
 (use-package buffer-move)
 (use-package ibuffer
-  :demand t
   :bind (([remap list-buffers] . #'ibuffer)))
+
+(use-package electric
+  :ensure nil
+  :custom ; “Prettier ‘quotes’”
+  (electric-quote-replace-double t)
+  (electric-quote-context-sensitive t)
+  :config (electric-quote-mode))
+
+(use-package elec-pair
+  :ensure t
+  :custom (electric-pair-skip-whitespace 'chomp)
+  :config (electric-pair-mode))
 
 ;; mode-line
 (line-number-mode)
@@ -118,9 +133,15 @@
 (setq mode-line-percent-position nil)
 
 ;; ediff
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq ediff-split-window-function (quote split-window-horizontally))
-(add-hook 'ediff-after-quit-hook-internal 'winner-undo)
+(use-package ediff
+  :ensure t
+  :custom
+  (ediff-keep-variants nil)
+  (ediff-show-clashes-only t)
+  (ediff-diff-options "-w")
+  (ediff-split-window-function 'split-window-horizontally)
+  (ediff-window-setup-function 'ediff-setup-windows-plain)
+  (add-hook 'ediff-after-quit-hook-internal 'winner-undo))
 
 ;; following windows splits
 (defun split-and-follow-horizontally ()
@@ -148,14 +169,42 @@
 
 ;; save cursor position
 (use-package saveplace
-  :demand t
   :config
   (save-place-mode +1))
+
+(global-unset-key (kbd "C-z"))
+(use-package undo-tree
+  :config
+  (setq undo-tree-enable-undo-in-region nil)
+  (setq undo-tree-history-directory-alist
+    `((".*" . ,temporary-file-directory)))
+  (setq undo-tree-auto-save-history t)
+  (global-undo-tree-mode +1))
 
 ;; highlight current line
 (use-package hl-line
   :demand t
   :config (global-hl-line-mode))
+
+(use-package flyspell
+  :ensure nil
+  :hook
+  (text-mode . flyspell-mode)
+  (prog-mode . flyspell-prog-mode)
+  :custom
+  (flyspell-mode-map (make-sparse-keymap) "Disable all flyspell bindings")
+  (ispell-program-name "aspell")
+  (ispell-extra-args '("--sug-mode=ultra")))
+
+(use-package flyspell-correct
+  :defines flyspell-mode-map
+  :requires flyspell
+  :config (bind-key [remap ispell-word] #'flyspell-correct-wrapper flyspell-mode-map))
+
+(use-package auto-correct
+  :delight
+  :hook (flyspell-mode . auto-correct-mode)
+  :custom (flyspell-use-global-abbrev-table-p t))
 
 ;; Tips for next keystroke
 (use-package which-key
@@ -164,7 +213,6 @@
   :blackout t)
 
 (use-package vterm
-  :demand t
   :straight nil)
 
 (use-package shell-pop
@@ -183,11 +231,11 @@
   :config
   (put 'dired-find-alternate-file 'disabled nil)
   (setq
-   dired-recursive-deletes 'always
-   dired-recursive-copies 'always
-   dired-dwim-target t
-   dired-listing-switches "-alhv --group-directories-first"
-   dired-no-confirm '(copy))
+    dired-recursive-deletes 'always
+    dired-recursive-copies 'always
+    dired-dwim-target t
+    dired-listing-switches "-alhv --group-directories-first"
+    dired-no-confirm '(copy))
   (define-key dired-mode-map (kbd "<left>") 'dired-up-directory)
   (define-key dired-mode-map (kbd "<right>") 'dired-find-alternate-file)
   (define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file)
@@ -206,8 +254,7 @@
 
 (use-package selectrum-prescient
   :straight (:host github :repo "raxod502/prescient.el"
-                   :files ("selectrum-prescient.el"))
-  :demand t
+              :files ("selectrum-prescient.el"))
   :after selectrum
   :config
   (selectrum-prescient-mode +1))
@@ -215,9 +262,9 @@
 (use-package projectile
   :bind-keymap* (("C-c p" . projectile-command-map))
   :bind (("s-n" . projectile-switch-project)
-         ( "s-/" . projectile-switch-to-buffer)
-         ("s-p" . projectile-find-file)
-         ("s-o" . projectile-switch-open-project))
+          ( "s-/" . projectile-switch-to-buffer)
+          ("s-p" . projectile-find-file)
+          ("s-o" . projectile-switch-open-project))
   :config
   (setq projectile-completion-system 'default)
   (projectile-mode +1))
@@ -226,8 +273,8 @@
   :demand t
   :config
   (setq flycheck-completion-system 'default
-        flycheck-idle-change-delay 1.0
-        flycheck-indication-mode 'left-fringe)
+    flycheck-idle-change-delay 1.0
+    flycheck-indication-mode 'left-fringe)
   (define-key flycheck-mode-map (kbd "M-n") 'flycheck-next-error)
   (define-key flycheck-mode-map (kbd "M-p") 'flycheck-previous-error)
   (global-flycheck-mode))
@@ -236,26 +283,23 @@
   :ensure hydra
   :init
   (global-set-key
-   (kbd "C-x a")
-   (defhydra toggle (:color blue)
-     "toggle"
-     ("a" abbrev-mode "abbrev")
-     ("s" flyspell-mode "flyspell")
-     ("d" toggle-debug-on-error "debug")
-     ("f" auto-fill-mode "fill")
-     ("t" toggle-truncate-lines "truncate")
-     ("w" whitespace-mode "whitespace")
-     ("q" nil "cancel"))))
+    (kbd "C-x a")
+    (defhydra toggle (:color blue)
+      "toggle"
+      ("a" abbrev-mode "abbrev")
+      ("s" flyspell-mode "flyspell")
+      ("d" toggle-debug-on-error "debug")
+      ("f" auto-fill-mode "fill")
+      ("t" toggle-truncate-lines "truncate")
+      ("w" whitespace-mode "whitespace")
+      ("q" nil "cancel"))))
 
 (use-package magit
   :bind (("C-x g" . magit-status))
   :config
   (add-hook 'after-save-hook 'magit-after-save-refresh-status t))
 
-(setq magit-status-margin
-      '(t "%Y-%m-%d %H:%M " magit-log-margin-width t 18))
 (use-package git-gutter
-  :ensure t
   :init
   (global-git-gutter-mode +1))
 
@@ -263,10 +307,9 @@
 
 
 (use-package git-timemachine
-  :ensure t
   )
 (defhydra hydra-git-gutter (:body-pre (git-gutter-mode 1)
-                                      :hint nil)
+                             :hint nil)
   "
   Git gutter:
     _j_: next hunk        _s_tage hunk     _q_uit
@@ -278,37 +321,37 @@
   ("j" git-gutter:next-hunk)
   ("k" git-gutter:previous-hunk)
   ("h" (progn (goto-char (point-min))
-              (git-gutter:next-hunk 1)))
+         (git-gutter:next-hunk 1)))
   ("l" (progn (goto-char (point-min))
-              (git-gutter:previous-hunk 1)))
+         (git-gutter:previous-hunk 1)))
   ("s" git-gutter:stage-hunk)
   ("r" git-gutter:revert-hunk)
   ("p" git-gutter:popup-hunk)
   ("R" git-gutter:set-start-revision)
   ("q" nil :color blue)
   ("Q" (progn (git-gutter-mode -1)
-              ;; git-gutter-fringe doesn't seem to
-              ;; clear the markup right away
-              (sit-for 0.1)
-              (git-gutter:clear))
-   :color blue))
+         ;; git-gutter-fringe doesn't seem to
+         ;; clear the markup right away
+         (sit-for 0.1)
+         (git-gutter:clear))
+    :color blue))
 
 (use-package company
-  :demand t
   :blackout t
-  :init (setq company-backends '((company-files company-keywords company-capf company-dabbrev-code company-etags company-dabbrev)))
+  :init
+  (global-company-mode)
+  (setq company-backends '((company-files company-keywords company-capf company-dabbrev-code company-etags company-dabbrev)))
   :config
   (setq company-idle-delay 0.3)
   (setq company-tooltip-limit 10)
   (setq company-minimum-prefix-length 2)
   (setq company-tooltip-align-annotations t)
-  (setq company-tooltip-flip-when-above t)
-  (global-company-mode))
+  (setq company-tooltip-flip-when-above t))
 
 (use-package multiple-cursors
   :bind
   (("C-c n" . mc/mark-next-like-this)
-   ("C-c p" . mc/mark-previous-like-this)))
+    ("C-c p" . mc/mark-previous-like-this)))
 
 (use-package expand-region
   :blackout t
@@ -321,14 +364,13 @@
   :config
   (wrap-region-global-mode t)
   (wrap-region-add-wrappers
-   '(
-     ("`" "`")
-     ("*" "*")
-     )))
+    '(
+       ("`" "`")
+       ("*" "*")
+       )))
 
 (use-package ctrlf
-  :demand t
-  :config
+  :init
   (ctrlf-mode +1))
 
 (use-package editorconfig
@@ -339,6 +381,10 @@
 (use-package nix-mode
   :config
   (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode)))
+
+(use-package toml-mode)
+(use-package yaml-mode)
+(use-package nginx-mode)
 
 (use-package server
   :ensure nil
@@ -356,13 +402,12 @@
 ;; theme
 (use-package ample-theme
   :init (progn (load-theme 'ample t t)
-               (load-theme 'ample-flat t t)
-               (load-theme 'ample-light t t)
-               (enable-theme 'ample-flat))
+          (load-theme 'ample-flat t t)
+          (load-theme 'ample-light t t)
+          (enable-theme 'ample-flat))
   :defer t
   :ensure t)
 
-;; smart-mode-line
 (use-package mood-line
   :demand t
   :config
